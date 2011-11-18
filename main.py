bc7fa39f91cb6b15c4b4d981f323133207760ec2
Jeffrey Posnick <jeffy@google.com>
2011-11-18 15:19:32 -0500

    Added a few changes to set up the templating and webapp environment.

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+import webapp2
+from webapp2_extras import jinja2
+
+class BaseHandler(webapp2.RequestHandler):
+  @webapp2.cached_property
+  def jinja2(self):
+    return jinja2.get_jinja2(app=self.app)
+
+  def render_template(self, filename, **template_args):
+    self.response.write(self.jinja2.render_template(filename, **template_args))
 
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+class IndexHandler(BaseHandler):
+  def get(self):
+    self.render_template("index.html")
 
 
-def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-    main()
+application = webapp2.WSGIApplication([('/', IndexHandler)], debug=True)
