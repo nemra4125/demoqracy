966acccf7a45c67c7f75dacf826c8ff0cd4bca55
Jeffrey Posnick <jeffy@google.com>
2011-11-22 10:57:38 -0500

    Added a handler for viewing election results.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from createelectionhandler import CreateElectionHandler
 from indexhandler import IndexHandler
 from printcandidateshandler import PrintCandidatesHandler
+from viewelectionhandler import ViewElectionHandler
 from votehandler import VoteHandler
 import webapp2
 
@@ -8,5 +9,6 @@ application = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/elections/create', CreateElectionHandler),
     ('/elections/([^/]+)/print', PrintCandidatesHandler),
+    ('/elections/([^/]+)/view', ViewElectionHandler),
     ('/([^/]+)/([^/]+)/vote', VoteHandler)
 ], debug=True)
