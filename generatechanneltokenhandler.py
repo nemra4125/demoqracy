f1947ebb2181edb7096ef03aaff55c47ce67dcff
Jeffrey Posnick <jeffy@google.com>
2011-11-22 23:36:41 -0500

    Added a datastore model to store active channel tokens, and a cron job to delete stale tokens.

--- generatechanneltokenhandler.py
+++ generatechanneltokenhandler.py
@@ -1,7 +1,7 @@
 from basehandler import BaseHandler
 from google.appengine.api import channel
 from google.appengine.api import users
-from model import Election
+from model import ChannelToken, Election
 from utils import WriteJsonResponse
 from webob.exc import HTTPUnauthorized
 import random
@@ -13,5 +13,6 @@ class GenerateChannelTokenHandler(BaseHandler):
     if election.owner != current_user:
       raise HTTPUnauthorized("You are not the owner of this election.")
     channel_id = "%s:%s" % (current_user.user_id(), random.random())
+    ChannelToken(channel_id=channel_id).put()
     WriteJsonResponse(self,
                       dict(channelToken=channel.create_channel(channel_id)))
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from createelectionhandler import CreateElectionHandler
 from generatechanneltokenhandler import GenerateChannelTokenHandler
 from indexhandler import IndexHandler
 from printcandidateshandler import PrintCandidatesHandler
+from purgetokenshandler import PurgeTokensHandler
 from viewelectionhandler import ViewElectionHandler
 from votehandler import VoteHandler
 import webapp2
 
 application = webapp2.WSGIApplication([
     ('/', IndexHandler),
+    ('/tasks/purge_tokens', PurgeTokensHandler),
     ('/elections/create', CreateElectionHandler),
     ('/elections/([^/]+)/print', PrintCandidatesHandler),
     ('/elections/([^/]+)/view', ViewElectionHandler),
