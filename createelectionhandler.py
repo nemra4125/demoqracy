dc149345df9310c1e190e666c64350b07551ab43
Jeffrey Posnick <jeffy@google.com>
2011-11-28 17:23:32 -0500

    The election creation code has been refactored so that it no longer relies on JS to build a hidden form field to contain a list of all candidates. Instead, it uses the webapp2 support for multiple parameters with the same name to retrieve all of them.

--- createelectionhandler.py
+++ createelectionhandler.py
@@ -3,7 +3,7 @@ from google.appengine.api import users
 from google.appengine.ext import db
 from model import Candidate, Election
 from utils import ProcessParams
-from webob.exc import HTTPUnauthorized
+from webob.exc import HTTPBadRequest, HTTPUnauthorized
 from webapp2_extras.appengine.users import login_required
 import datetime
 
@@ -18,6 +18,9 @@ class CreateElectionHandler(BaseHandler):
     params = ProcessParams(request=self.request,
                            optional_params=["start_ts", "end_ts"],
                            required_params=["title", "candidates"])
+    if (not isinstance(params["candidates"], list)
+      or len(params["candidates"]) < 2):
+      raise HTTPBadRequest("At least two candidates are required.")
     # NOTE: Cross-group transcations requires using the high-replication
     # datastore. Add the --high_replication command line flag to
     # dev_appserver.py in the development environment.
@@ -31,9 +34,10 @@ class CreateElectionHandler(BaseHandler):
     if "end_ts" in params:
       election.end = datetime.datetime.fromtimestamp(params["end_ts"])
     election.put()
-    for name in params["candidates"].split("||"):
-      candidate = Candidate(parent=election, name=name)
-      candidate.put()
+    for name in params["candidates"]:
+      if name:
+        candidate = Candidate(parent=election, name=name)
+        candidate.put()
     self.render_template("create.html",
                          render_form=False,
                          election_title=election.title,
