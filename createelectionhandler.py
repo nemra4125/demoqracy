8b4464a8c3e44c558191fb3492440cb31047a9cd
Jeffrey Posnick <jeffy@google.com>
2011-11-28 16:39:23 -0500

    Moved the election and candidate creation process to a datastore transaction.

--- createelectionhandler.py
+++ createelectionhandler.py
@@ -1,5 +1,6 @@
 from basehandler import BaseHandler
 from google.appengine.api import users
+from google.appengine.ext import db
 from model import Candidate, Election
 from utils import ProcessParams
 from webob.exc import HTTPUnauthorized
@@ -10,14 +11,20 @@ class CreateElectionHandler(BaseHandler):
   @login_required
   def get(self):
     self.render_template("create.html", render_form=True)
-  
+
   def post(self):
     if users.get_current_user() is None:
       raise HTTPUnauthorized("You must be logged in to create a new election.")
     params = ProcessParams(request=self.request,
                            optional_params=["start_ts", "end_ts"],
                            required_params=["title", "candidates"])
-    #TODO: Move this to a transaction.
+    # NOTE: Cross-group transcations requires using the high-replication
+    # datastore. Add the --high_replication command line flag to
+    # dev_appserver.py in the development environment.
+    db.run_in_transaction_options(db.create_transaction_options(xg=True),
+                                  self.CreateElectionAndCandidates, params)
+
+  def CreateElectionAndCandidates(self, params):
     election = Election(title=params["title"], owner=users.get_current_user())
     if "start_ts" in params:
       election.start = datetime.datetime.fromtimestamp(params["start_ts"])
@@ -27,7 +34,6 @@ class CreateElectionHandler(BaseHandler):
     for name in params["candidates"].split("||"):
       candidate = Candidate(parent=election, name=name)
       candidate.put()
-      
     self.render_template("create.html",
                          render_form=False,
                          election_title=election.title,
