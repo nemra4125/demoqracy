66397d0391a24cff1595f5313ccdc0867dea5398
akarp <akarp@google.com>
2011-11-30 18:26:49 -0500

    made the vote page more sparse; added cancel button

--- votehandler.py
+++ votehandler.py
@@ -9,9 +9,23 @@ class VoteHandler(BaseHandler):
   def get(self, election_id, candidate_id):
     election, candidate = self.ValidateElectionAndCandidate(
       election_id, candidate_id)
-    self.render_template("vote.html", name=candidate.name)
+    
+    message = ""
+    canvote = True
+    
+    if election.HasAlreadyVoted(users.get_current_user()):
+      message = "You've already voted in this election."
+      canvote = False
+      #raise HTTPBadRequest("You've already voted in this election.")
+    
+    self.render_template("vote.html", title=election.title, 
+                         name=candidate.name, message=message, 
+                         canvote=canvote)
 
   def post(self, election_id, candidate_id):
+    if self.request.get("cancel_button") == "True":
+       self.render_template("vote.html", message="Your vote has been discarded")
+       return
     current_user = users.get_current_user()
     if current_user is None:
       raise HTTPUnauthorized("You must be logged in to vote.")
@@ -20,7 +34,7 @@ class VoteHandler(BaseHandler):
     voter_id = election.GenerateVoterId(current_user)
     Vote(parent=candidate, voter=voter_id, election=str(election.key())).put()
     self.NotifyChannels(election)
-    self.render_template("thanks.html", name=candidate.name)
+    self.render_template("vote.html", canvote=False, message="Thanks! Your vote for %s has been registered." % candidate.name)
 
   def NotifyChannels(self, election):
     message = election.GetElectionStateAsJson()
@@ -36,8 +50,5 @@ class VoteHandler(BaseHandler):
     if candidate is None:
       raise HTTPBadRequest("Couldn't find candidate with id '%s' "
                            "in election id '%s'." % (candidate_id, election_id))
-    if voter is None:
-      voter = users.get_current_user()
-    if election.HasAlreadyVoted(voter):
-      raise HTTPBadRequest("You've already voted in this election.")
+  
     return election, candidate
