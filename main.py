5ce42a713519f3a88709383ee4ca47f19e919f11
jkleinert <jkleinert@dhcp-172-26-103-226.nyc.corp.google.com>
2011-12-13 16:39:34 -0500

    Adding (sandbox) in-app payments and adsense code

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from createelectionhandler import CreateElectionHandler
 from generatechanneltokenhandler import GenerateChannelTokenHandler
 from historyhandler import HistoryHandler
 from indexhandler import IndexHandler
+from postbackverifyhandler import PostbackVerifyHandler
 from printcandidateshandler import PrintCandidatesHandler
 from purgetokenshandler import PurgeTokensHandler
 from viewelectionhandler import ViewElectionHandler
+from updateelectionhandler import UpdateElectionHandler
 from myelectionshandler import MyElectionsHandler
 from webvotehandler import WebVoteHandler
 from statichandler import StaticHandler
@@ -19,11 +21,13 @@ application = webapp2.WSGIApplication([
     ('/(privacy)', StaticHandler),
     ('/(about)', StaticHandler),
     ('/(how-it-works)', StaticHandler),
+    ('/(postback-verify)', PostbackVerifyHandler),
     ('/vote/(\d+)', WebVoteHandler),
     ('/elections/create', CreateElectionHandler),
     ('/elections/(\d+)/print', PrintCandidatesHandler),
     ('/elections/(\d+)/view', ViewElectionHandler),
     ('/elections/(\d+)/history', HistoryHandler),
+    ('/elections/(\d+)/update', UpdateElectionHandler),
     ('/elections/(\d+)/generate_channel_token', GenerateChannelTokenHandler),
     ('/(\d+)/(\d+)/vote', VoteHandler),
 ], debug=True)
---
+++ updateelectionhandler.py
@@ -0,0 +1,33 @@
+from basehandler import BaseHandler
+from google.appengine.api import users
+from google.appengine.ext import db
+from model import Candidate, Election
+from utils import ProcessParams
+from webob.exc import HTTPBadRequest, HTTPUnauthorized
+from webapp2_extras.appengine.users import login_required
+import datetime
+
+class UpdateElectionHandler(BaseHandler):
+
+  def post(self, election_id):
+    if users.get_current_user() is None:
+      raise HTTPUnauthorized("You must be logged in to create a new election.")
+    params = ProcessParams(request=self.request,
+                           optional_params=["ads_enabled"],
+                           required_params=[])
+
+    election = Election.get_by_id(long(election_id))
+    if election is None or election.owner != users.get_current_user():
+      raise HTTPUnauthorized("You are not the owner of this election.")
+
+    if params["ads_enabled"] == "True":
+      election.ads_enabled = True
+    else:
+      election.ads_enabled = False
+    election.put()
+    self.render_template("view.html",
+                         election_state=election.GetElectionStateAsJson(),
+                         title=election.title,
+                         election_id=election_id,
+                         total_votes=election.GetElectionHistory()[0],
+                         ads_enabled=election.ads_enabled)
