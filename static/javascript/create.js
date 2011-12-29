9ab9dc111573199f24a59ca2265d883c28c272ec
Jeffrey Posnick <jeffy@google.com>
2011-12-29 13:12:38 -0500

    Changes to allow for times (as well as dates) for election beginnings and endings. Also enforce checking whether the election is active before allowing people to vote.

--- static/javascript/create.js
+++ static/javascript/create.js
@@ -1,78 +1,62 @@
 (function() {
-  
-  /**
-   * Shared vars
-   */
-  var long_date_format = "MM d, yy"  
-  
-  /**
-   * Handle create election submissions
-   */ 
-  $(function() {
-    $('#finish_create_election_button')
-        .click(
-            function() {
-              var form = $(this).closest('form');
-              
-              // Start date timestamp 
-              var start_ts = $("#start_date_field").val() != "" ? 
-                  Date.parse($("#start_date_field").val()) / 1000 : 0;
-              // End date timestamp
-              var end_ts = $("#end_date_field").val() != "" ? 
-                  Date.parse($("#end_date_field").val()) / 1000 : 0;
-              
-              // Validate
-              var errors = [];
-              if (end_ts < start_ts) {
-                errors.push("End date cannot come before start date");
-              }
-              if ($("#title_field").val() == "") {
-                errors.push("Title must not be blank");
-              }
-              var valid_candidate_count = 0;
-              $('.candidate').each(function() {
-                if ($(this).val() != "") {
-                  valid_candidate_count++;
-                }
-              });
-              if (valid_candidate_count < 2) {
-                errors.push("You must provide at least two candidates");
-              }
-              // Add errors to page
-              if (errors.length > 0) {
-                $("#create_errors").empty();
-                for (i=0; i<errors.length; i++) {
-                  $("#create_errors").append("<li>" + errors[i] + "</li>");
-                }
-                $("#create_errors").css("display", "block");
-                window.location = "#create_errors";
-                return false;
-              }
-              
-              // Convert dates to timestamps and put them in hidden fields
-              var hiddenStartTimestamp = $
-              .sprintf(
-                  '<input type="hidden" name="start_ts" id="start_ts" value="%s">',
-                  start_ts);
-              $(hiddenStartTimestamp).appendTo(form);
-          
-              var hiddenEndTimestamp = $
-              .sprintf(
-                  '<input type="hidden" name="end_ts" id="end_ts" value="%s">',
-                  end_ts);
-              $(hiddenEndTimestamp).appendTo(form);
-              
-              form.submit();
-              
-              // Cancel click prop
-              return false;
-            });
-  });
+  function remove_candidate_row() {
+    $(this).parent().remove();
+    // If only two remaining, hide remove buttons
+    if ($('.candidate_remove').length == 2) {
+      $('.candidate_remove').css('display', 'none');
+    }
+  }
 
-  /**
-   * Add new candidate link
-   */ 
   $(function() {
+    $('#finish_create_election_button').click(function() {
+      var form = $(this).closest('form');
+
+      // Start date timestamp
+      var start_ts = $("#start_date_field").val() != "" ? Date.parse($("#start_date_field").val()) / 1000 : 0;
+      // End date timestamp
+      var end_ts = $("#end_date_field").val() != "" ? Date.parse($("#end_date_field").val()) / 1000 : 0;
+
+      // Validate
+      var errors = [];
+      if (end_ts < start_ts) {
+        errors.push("End date cannot come before start date");
+      }
+      if ($("#title_field").val() == "") {
+        errors.push("Title must not be blank");
+      }
+      var valid_candidate_count = 0;
+      $('.candidate').each(function() {
+        if ($(this).val() != "") {
+          valid_candidate_count++;
+        }
+      });
+      if (valid_candidate_count < 2) {
+        errors.push("You must provide at least two candidates");
+      }
+      // Add errors to page
+      if (errors.length > 0) {
+        $("#create_errors").empty();
+        for (i = 0; i < errors.length; i++) {
+          $("#create_errors").append("<li>" + errors[i] + "</li>");
+        }
+        $("#create_errors").css("display", "block");
+        window.location = "#create_errors";
+        return false;
+      }
+
+      // Convert dates to timestamps and put them in hidden fields
+      var hiddenStartTimestamp = $.sprintf('<input type="hidden" name="start_ts" id="start_ts" value="%s">', start_ts);
+      $(hiddenStartTimestamp).appendTo(form);
+
+      var hiddenEndTimestamp = $.sprintf('<input type="hidden" name="end_ts" id="end_ts" value="%s">', end_ts);
+      $(hiddenEndTimestamp).appendTo(form);
+
+      form.submit();
+
+      // Cancel click prop
+      return false;
+    });
+
     $('#add_new_candidate_form_row').click(function() {
       var last_li = $('.candidate:last').parent();
       last_li.after(last_li.clone());
@@ -84,39 +68,12 @@
       // cancel click prop
       return false;
     });
-  });
-  
-  /**
-   * Handler for removing candidate rows 
-   */ 
-  function remove_candidate_row(event) {
-    $(this).parent().remove();
-    // If only two remaining, hide remove buttons
-    if ($('.candidate_remove').length == 2) {
-      $('.candidate_remove').css('display', 'none');
-    }
-  }
-  
-  /**
-   * Set up initial handlers for removing candidate rows
-   */ 
-  $(function() {
+
     $('.candidate_remove').click(remove_candidate_row);
-  });
-  
-  /**
-   * Set up date pickers
-   */
-  $(function() {
-    $('#start_date_field').datepicker({
+
+    $('.needsDateTimePicker').datetimepicker({
       inline: true,
-      dateFormat: long_date_format
-    });
-    
-    $('#end_date_field').datepicker({
-      inline: true,
-      dateFormat: long_date_format
+      dateFormat: "MM d, yy"
     });
   });
-  
 })();
