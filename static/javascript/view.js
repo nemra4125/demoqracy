d93da8cdc5c52af5b20a1e4d6fb0738d1c51b4c6
Jeffrey Posnick <jeffy@google.com>
2012-01-10 17:34:37 -0500

    Refactored the vote history chart so that it's now on the main view page.

--- static/javascript/view.js
+++ static/javascript/view.js
@@ -1,15 +1,59 @@
 (function() {
-  var barChart;
-  var pieChart;
+  var liveChart;
+  var dataTables = [];
+  var queuedIndex = 0;
+  var historyChart;
 
-  window.drawChart = function(electionState) {
-    $('#charts').hide();
+  function updateHistoryChart(index) {
+    index = parseInt(index);
 
-    if (barChart == null) {
-      barChart = new google.visualization.ColumnChart(document.getElementById("bar-chart"));
+    if (historyChart == null) {
+      historyChart = new google.visualization.PieChart(document.getElementById("history-chart"));
     }
-    if (pieChart == null) {
-      pieChart = new google.visualization.PieChart(document.getElementById("pie-chart"));
+
+    if (dataTables[index] != null) {
+      queuedIndex = null;
+      historyChart.draw(dataTables[index], {title: $.sprintf('Vote percentage after %d vote%s.', index + 1, index == 0 ? '' : 's')});
+    } else {
+      queuedIndex = index;
+    }
+  }
+
+  function initializeWebWorker(electionHistory, candidates) {
+    var worker = new Worker('/static/javascript/view-web-worker.js');
+    $(worker).on('message', function(event) {
+      var data = event.originalEvent.data;
+
+      dataTables[data.index] = new google.visualization.DataTable(data.dataTable);
+
+      if (data.index == 0) {
+        $('#history-range').removeAttr('disabled');
+        $('#play').removeAttr('disabled');
+      }
+
+      if (data.index == queuedIndex) {
+        updateHistoryChart(0);
+      }
+    });
+
+    worker.postMessage({electionHistory: electionHistory, candidates: candidates});
+  }
+
+  function play(index) {
+    $('#history-range').val(index);
+
+    updateHistoryChart(index);
+
+    if (index < $('#history-range').attr('max')) {
+      setTimeout(function() {
+        play(index + 1);
+      }, 1000);
+    }
+  }
+
+  window.updateLiveChart = function(electionState) {
+    if (liveChart == null) {
+      liveChart = new google.visualization.ColumnChart(document.getElementById("bar-chart"));
     }
 
     var numTotalVotes = 0;
@@ -26,11 +70,20 @@
     });
 
     if (numTotalVotes > 0) {
-      barChart.draw(data, {colors: ["#caeefc"]});
-      pieChart.draw(data);
-
-      $('#charts').fadeIn('slow');
+      liveChart.draw(data, {colors: ["#caeefc"]});
     }
+  }
+
+  window.initializeHistoryChart = function() {
+    initializeWebWorker(window.electionHistory, window.candidates);
+
+    $('#history-range').change(function() {
+      updateHistoryChart($(this).val());
+    });
+
+    $('#play').click(function() {
+      play(0);
+    });
   }
 
   function openChannelConnection(electionId) {
@@ -41,7 +94,7 @@
         var channel = new goog.appengine.Channel(json.channelToken);
         var socket = channel.open();
         socket.onmessage = function(message) {
-          drawChart($.parseJSON(message.data));
+          window.updateLiveChart($.parseJSON(message.data));
         };
         socket.onerror = function(error) {
           console.log('Channel connection error:');
