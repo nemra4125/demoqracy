84ea961e7d089da2c6ee311276a8196162d21967
Jeffrey Posnick <jeffy@google.com>
2012-01-10 17:03:22 -0500

    Fixed something that relied on the deprecated "public" property of an election.

--- generatechanneltokenhandler.py
+++ generatechanneltokenhandler.py
@@ -1,16 +1,10 @@
 from basehandler import BaseHandler
 from channelapihelper import ChannelApiHelper
-from google.appengine.api import users
 from model import Election
 from utils import WriteJsonResponse
-from webob.exc import HTTPUnauthorized
 
 class GenerateChannelTokenHandler(BaseHandler):
   def get(self, election_id):
     election = Election.get_by_id(long(election_id))
-    current_user = users.get_current_user()
-    if not election.public_results and election.owner != current_user:
-      raise HTTPUnauthorized("You are not the owner of this election.")
     channel_token = ChannelApiHelper(election).CreateChannel()
-    WriteJsonResponse(self,
-                      dict(channelToken=channel_token))
+    WriteJsonResponse(self, dict(channelToken=channel_token))
