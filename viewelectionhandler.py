e09ab89740d7b560528c47a67fae38fd57e02fb2
Jeffrey Posnick <jeffy@google.com>
2012-01-10 14:02:58 -0500

    No longer require logging in to view an election's results.

--- viewelectionhandler.py
+++ viewelectionhandler.py
@@ -10,7 +10,6 @@ from webapp2_extras.appengine.users import login_required
 from webob.exc import HTTPUnauthorized
 
 class ViewElectionHandler(BaseHandler):
-  @login_required
   def get(self, election_id):
     election = Election.get_by_id(long(election_id))
     if election is None:
