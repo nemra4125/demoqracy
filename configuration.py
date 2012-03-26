73e186d9db42b052982076d17fd9ae515b1be54e
Jeffrey Posnick <jeffy@google.com>
2012-03-26 14:34:42 -0400

    Moved the Checkout API configuration options into a new configuration.py file, which will be checked in with default dummy values. Added in logic to look for a custom_configuration.py file that will override those default values. Don't check this file into version control!

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Either update this file, or create a copy named "custom_configuration.py"
+
+# Used by the Checkout API.
+SELLER_ID = "REPLACE ME!"
+SELLER_SECRET = "REPLACE ME!"
--- postbackverifyhandler.py
+++ postbackverifyhandler.py
@@ -1,28 +1,24 @@
 from basehandler import BaseHandler
-from google.appengine.api import users
-from google.appengine.ext import db
-from model import Candidate, Election
-from utils import ProcessParams
-from webob.exc import HTTPBadRequest, HTTPUnauthorized
-from webapp2_extras.appengine.users import login_required
-from sellerinfo import SELLER_ID
-from sellerinfo import SELLER_SECRET
-import logging
+from model import Election
+try:
+  import custom_configuration as configuration
+except ImportError:
+  import configuration
 
 class PostbackVerifyHandler(BaseHandler):
-
   def post(self):
     """Handles post request."""
     encoded_jwt = self.request.get('jwt', None)
     if encoded_jwt is not None:
       # jwt.decode won't accept unicode, cast to str
       # http://github.com/progrium/pyjwt/issues/4
-      decoded_jwt = jwt.decode(str(encoded_jwt), SELLER_SECRET)
+      decoded_jwt = jwt.decode(str(encoded_jwt), configuration.SELLER_SECRET)
 
       # Only update datastore and respond to Google if we have all the values
       # we need. If not, the payment will not go through since the postback
       # will not have a response to validate
-      if decoded_jwt['iss'] == 'Google' and decoded_jwt['aud'] == SELLER_ID:
+      if (decoded_jwt['iss'] == 'Google' and
+        decoded_jwt['aud'] == configuration.SELLER_ID):
         if ('response' in decoded_jwt and
             'orderId' in decoded_jwt['response'] and
             'request' in decoded_jwt):
